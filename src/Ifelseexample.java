562e216e3a47c915d6b5b5439a2d37542ae23a36
Lenovo <shareef.chilakala16@gmail.com>
2024-03-16 12:24:27 +0530

    git commit

---
+++ src/Ifelseexample.java
@@ -0,0 +1,23 @@
+public class Ifelseexample {
+    public static void main(String args[]) {
+
+
+        int salary = 65;
+        if (salary < 60) {
+            System.out.println("not eligible");
+        } else if (salary < 65){
+            System.out.println("use");
+        }
+    else if (salary > 70) {
+            System.out.println("avg");
+        }
+            else if (salary > 80) {
+                System.out.println("perfect");
+            } else if (salary > 90) {
+                System.out.println("eligible");
+            }else {
+                System.out.println("perfect");
+            }
+        }
+    }
+
