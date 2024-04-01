44bfc8b4271936be4d545b27eb89d3006de4088f
Lenovo <shareef.chilakala16@gmail.com>
2024-04-01 17:39:26 +0530

    Practice

---
+++ src/Exceptions/Run.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class Run {
+    public static void main(String args[]) {
+        System.out.println("program execution start");
+        int fnumber = 9;
+        int snumber = 0;
+        int result;
+        result = fnumber / snumber;
+        System.out.println("output is:" + result);
+        System.out.println("program execution end");
+    }
+}
