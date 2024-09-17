3869d375118960df6a12695808c4368eb5fa1cca
Shareef <163128705+mohammadshareefc@users.noreply.github.com>
2024-09-17 10:19:15 +0530

    Delete Crud API/Crud-API directory

--- Crud API/Crud-API/src/main/java/com/example/Crud/API/repository/Studentrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Crud.API.repository;
-
-import com.example.Crud.API.entity.Students;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Studentrepo extends JpaRepository<Students, Integer> {
-}
